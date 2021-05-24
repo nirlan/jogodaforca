fe910475392c744035f8fecaea02fc60bca8d4c6
Nirlan Souza <nirlan858@gmail.com>
2021-05-24 12:35:15 -0300

    Implementação inicial de do  while loop e char array

--- forca.c
+++ forca.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    
+    char palavrasecreta[20];
+
+    sprintf(palavrasecreta, "MELANCIA");
+    printf("%s", palavrasecreta);
+
+    int acertou = 0;
+    int enforcou = 0;
+
+    do {
+
+    } while(!acertou && !enforcou);
 }
