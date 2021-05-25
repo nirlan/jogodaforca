877a61eeb63568891274bb491de6494d02c61886
Nirlan Souza <nirlan858@gmail.com>
2021-05-25 00:17:46 -0300

    Varredura da palavra secreta em busca de chutes corretos

--- forca.c
+++ forca.c
@@ -1,15 +1,48 @@
 #include <stdio.h>
+#include <string.h>
+#include <locale.h>
 
 int main() {
+    setlocale(LC_ALL, "Portuguese");
     char palavrasecreta[20];
 
     sprintf(palavrasecreta, "MELANCIA");
-    printf("%s", palavrasecreta);
 
     int acertou = 0;
     int enforcou = 0;
 
-    do {
+    char chutes[26];
+    int tentativas;
 
+    do {
+        
+        for(int i = 0; i < strlen(palavrasecreta); i++) {
+            
+            int achou = 0;
+
+            for(int j = 0; j < tentativas; j++) {
+            
+                if(chutes[j] == palavrasecreta[i]) {
+
+                    achou = 1;
+                    break;
+                }
+            }
+
+            if(achou) {
+                printf("%c ", palavrasecreta[i]);
+            } else {
+                printf("_ ");
+            }
+        }
+        
+        printf("\n");
+
+        char chute;
+        scanf(" %c", &chute);
+
+        chutes[tentativas] = chute;
+        tentativas++;
+        
     } while(!acertou && !enforcou);
 }
