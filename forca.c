9bf66484b1c8f5b98e1f3f93dc85c48483d11282
Nirlan Souza <nirlan858@gmail.com>
2021-05-28 19:38:47 -0300

    Implementada a função adicionapalavra()

--- forca.c
+++ forca.c
@@ -40,6 +40,41 @@ void desenhaforca() {
 
 }
 
+void adicionapalavra() {
+    char quer;
+
+    printf("Você deseja adicionar uma nova palavra no jogo? (S/N)\n");
+    scanf(" %c", &quer);
+
+    if(quer == 'S') {
+
+        char novapalavra[20];
+        printf("Qual a nova palavra? ");
+        scanf("%s", novapalavra);
+
+        FILE* f;
+        
+        f = fopen("palavras.txt", "r+");
+        if(f == 0) {
+            printf("Desculpe, banco de dados não disponível\n\n");
+            exit(1);
+        }
+
+        int qtd;
+        fscanf(f, "%d", &qtd);
+        qtd++;
+
+        fseek(f, 0, SEEK_SET);
+        fprintf(f, "%d", qtd);
+
+        fseek(f, 0, SEEK_END);
+        fprintf(f, "\n%s", novapalavra)      ;
+
+        fclose(f);
+        
+    }
+}
+
 void escolhepalavra() {
     FILE* f;
 
@@ -122,4 +157,6 @@ int main() {
         chuta();
 
     } while(!acertou() && !enforcou());
+
+    adicionapalavra();
 }
